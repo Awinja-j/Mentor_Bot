41dcaaa2b68ac20b94b394a8e310ca0095e78c73
Awinja-Andela <joan.awinja@andela.com>
2017-11-02 19:55:00 +0300

    changed db

--- app2/settings.py
+++ app2/settings.py
@@ -6,6 +6,6 @@ FB_ACCESS_TOKEN = "EAAcLyLNtP4YBAAUaQRrEdYx3TuZAZC9fTcbmHv0OUZBp4nI7PeZCEvtgdu5k
 
 # configuration for the database url.
 DATABASE_URL = os.getenv('DATABASE_URL',
-                         'postgres://jus_machungwa@localhost/bot_db')
+                         'postgres://sinshsojtiexeq:edc5c508a2bc4c375d450a395a86851801ba59a1a3700c8f9fad0a945389eaff@ec2-50-16-228-232.compute-1.amazonaws.com:5432/d1n5ejp83fah5t')
 APP_SECRET_KEY = os.getenv('SECRET_KEY',
                            'this-really-needs-to-be-changed')
