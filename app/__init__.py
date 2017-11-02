2a9238ca162f1ebe59b7e139782d0091c93deb61
Awinja-Andela <joan.awinja@andela.com>
2017-11-02 21:02:17 +0300

    changed db path

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,11 @@
+import os
+import inspect
+import sys
+currentdir = os.path.dirname(os.path.abspath(
+    inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0, parentdir)
+
 from flask import Flask
 from flask_restful import Api
 from config import config
--- manage.py
+++ manage.py
@@ -1,3 +1,10 @@
+import os
+import inspect
+import sys
+currentdir = os.path.dirname(os.path.abspath(
+    inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0, parentdir)
 from flask_script import Manager, Shell
 from flask_migrate import Migrate, MigrateCommand
 from app.models.models import db
